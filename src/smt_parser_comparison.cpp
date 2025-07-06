b0d13998ea083bade84593a52f2c35727d4728d9
Fuqi Jia <jiafq@ios.ac.cn>
2025-07-06 20:38:24 +0800

    fix bugs for unique point

--- src/smt_parser_comparison.cpp
+++ src/smt_parser_comparison.cpp
@@ -14,7 +14,7 @@ namespace SMTComparison {
 std::string exec(const std::string& cmd) {
     std::array<char, 4096> buffer;
     std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    std::unique_ptr<FILE, int(*)(FILE*)> pipe(popen(cmd.c_str(), "r"), pclose);
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
