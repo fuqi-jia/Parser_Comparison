8c07872a3b4406fc963309660a209024c3007bad
Fuqi Jia <jiafq@ios.ac.cn>
2025-07-08 17:20:18 +0800

    Update parallel_pysmt.sh

--- parallel_pysmt.sh
+++ parallel_pysmt.sh
@@ -2,5 +2,5 @@ nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremen
 nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_IDL/ -o results/pysmt/QF_IDL.csv > /dev/null 2>/dev/null &
 nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_LIA/ -o results/pysmt/QF_LIA.csv > /dev/null 2>/dev/null &
 nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_NIA/ -o results/pysmt/QF_NIA.csv > /dev/null 2>/dev/null &
-nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_LRA/ -o results/pysmt/QF_LRA.csv > /dev/null 2>/dev/null &
-nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_NRA/ -o results/pysmt/QF_NRA.csv > /dev/null 2>/dev/null &
+# nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_LRA/ -o results/pysmt/QF_LRA.csv > /dev/null 2>/dev/null &
+# nohup ./build/smt_parser_comparison batch -p pysmt -d ../benchmarks/non-incremental/QF_NRA/ -o results/pysmt/QF_NRA.csv > /dev/null 2>/dev/null &
