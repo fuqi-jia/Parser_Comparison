5d53c1c4ba20ff0a446b5e0e9e130907e89cff9a
Fuqi Jia <jiafq@ios.ac.cn>
2025-07-10 10:06:52 +0800

    upload unzipped parsers

---
+++ external/swi_prolog/smtlib/pack.pl
@@ -0,0 +1,8 @@
+name(smtlib).
+title('SMT-LIB parser for SWI-Prolog').
+version('0.0.6').
+author('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
+maintainer('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
+packager('Jose Antonio Riaza Valverde', 'riaza.valverde@gmail.com').
+home('https://github.com/jariazavalverde/prolog-smtlib').
+download('https://github.com/jariazavalverde/prolog-smtlib').
---
+++ zip.sh
@@ -0,0 +1,172 @@
+zip -r part1.zip part1
+zip -r part2.zip part2
+zip -r part3.zip part3
+zip -r part4.zip part4
+zip -r part5.zip part5
+zip -r part6.zip part6
+zip -r part7.zip part7
+zip -r part8.zip part8
+zip -r part9.zip part9
+zip -r part10.zip part10
+zip -r part11.zip part11
+zip -r part12.zip part12
+zip -r part13.zip part13
+zip -r part14.zip part14
+zip -r part15.zip part15
+zip -r part16.zip part16
+zip -r part17.zip part17
+zip -r part18.zip part18
+zip -r part19.zip part19
+zip -r part20.zip part20
+zip -r part21.zip part21
+zip -r part22.zip part22
+zip -r part23.zip part23
+zip -r part24.zip part24
+zip -r part25.zip part25
+zip -r part26.zip part26
+zip -r part27.zip part27
+zip -r part28.zip part28
+zip -r part29.zip part29
+zip -r part30.zip part30
+zip -r part31.zip part31
+zip -r part32.zip part32
+zip -r part33.zip part33
+zip -r part34.zip part34
+zip -r part35.zip part35
+zip -r part36.zip part36
+zip -r part37.zip part37
+zip -r part38.zip part38
+zip -r part39.zip part39
+zip -r part40.zip part40
+zip -r part41.zip part41
+zip -r part42.zip part42
+zip -r part43.zip part43
+zip -r part44.zip part44
+zip -r part45.zip part45
+zip -r part46.zip part46
+zip -r part47.zip part47
+zip -r part48.zip part48
+zip -r part49.zip part49
+zip -r part50.zip part50
+zip -r part51.zip part51
+zip -r part52.zip part52
+zip -r part53.zip part53
+zip -r part54.zip part54
+zip -r part55.zip part55
+zip -r part56.zip part56
+zip -r part57.zip part57
+zip -r part58.zip part58
+zip -r part59.zip part59
+zip -r part60.zip part60
+zip -r part61.zip part61
+zip -r part62.zip part62
+zip -r part63.zip part63
+zip -r part64.zip part64
+zip -r part65.zip part65
+zip -r part66.zip part66
+zip -r part67.zip part67
+zip -r part68.zip part68
+zip -r part69.zip part69
+zip -r part70.zip part70
+zip -r part71.zip part71
+zip -r part72.zip part72
+zip -r part73.zip part73
+zip -r part74.zip part74
+zip -r part75.zip part75
+zip -r part76.zip part76
+zip -r part77.zip part77
+zip -r part78.zip part78
+zip -r part79.zip part79
+zip -r part80.zip part80
+zip -r part81.zip part81
+zip -r part82.zip part82
+zip -r part83.zip part83
+zip -r part84.zip part84
+zip -r part85.zip part85
+
+
+unzip part1.zip
+unzip part2.zip
+unzip part3.zip
+unzip part4.zip
+unzip part5.zip
+unzip part6.zip
+unzip part7.zip
+unzip part8.zip
+unzip part9.zip
+unzip part10.zip
+unzip part11.zip
+unzip part12.zip
+unzip part13.zip
+unzip part14.zip
+unzip part15.zip
+unzip part16.zip
+unzip part17.zip
+unzip part18.zip
+unzip part19.zip
+unzip part20.zip
+unzip part21.zip
+unzip part22.zip
+unzip part23.zip
+unzip part24.zip
+unzip part25.zip
+unzip part26.zip
+unzip part27.zip
+unzip part28.zip
+unzip part29.zip
+unzip part30.zip
+unzip part31.zip
+unzip part32.zip
+unzip part33.zip
+unzip part34.zip
+unzip part35.zip
+unzip part36.zip
+unzip part37.zip
+unzip part38.zip
+unzip part39.zip
+unzip part40.zip
+unzip part41.zip
+unzip part42.zip
+unzip part43.zip
+unzip part44.zip
+unzip part45.zip
+unzip part46.zip
+unzip part47.zip
+unzip part48.zip
+unzip part49.zip
+unzip part50.zip
+unzip part51.zip
+unzip part52.zip
+unzip part53.zip
+unzip part54.zip
+unzip part55.zip
+unzip part56.zip
+unzip part57.zip
+unzip part58.zip
+unzip part59.zip
+unzip part60.zip
+unzip part61.zip
+unzip part62.zip
+unzip part63.zip
+unzip part64.zip
+unzip part65.zip
+unzip part66.zip
+unzip part67.zip
+unzip part68.zip
+unzip part69.zip
+unzip part70.zip
+unzip part71.zip
+unzip part72.zip
+unzip part73.zip
+unzip part74.zip
+unzip part75.zip
+unzip part76.zip
+unzip part77.zip
+unzip part78.zip
+unzip part79.zip
+unzip part80.zip
+unzip part81.zip
+unzip part82.zip
+unzip part83.zip
+unzip part84.zip
+unzip part85.zip
